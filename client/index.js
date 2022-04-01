be8bfbfd441666df715aae3d6ac33cb4337453c9
Pavel Abduramanov <pabduramanov@i-doit.com>
2022-04-01 10:39:04 +0200

    Extend to use Server Stream

--- client/index.js
+++ client/index.js
@@ -1,10 +1,34 @@
 const grpc = require('@grpc/grpc-js');
+const readline = require('readline');
 const { ChatService } = require('./getService');
 const REMOTE_SERVER = '0.0.0.0:50051';
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const client = new ChatService(REMOTE_SERVER, grpc.credentials.createInsecure());
 
-client.send({
-  user: 'Max',
-  text: 'Mustermann',
-}, (error, response) => console.log(response));
+const startChat = user => {
+  console.log(user);
+
+  const channel = client.join({ user });
+
+  channel.on('data', ({ user: author, text }) => {
+    if (author === user) {
+      return;
+    }
+    console.log(`${author}: ${text}`);
+  });
+
+  rl.on('line', text =>
+    client.send({
+      user,
+      text,
+    }, (error, response) => `> ${response.text}`));
+};
+
+rl.question('What\'s your name?', answer => {
+  startChat(answer);
+});
--- server/index.js
+++ server/index.js
@@ -3,10 +3,32 @@ const { ChatService } = require('./getService');
 
 const server = new grpc.Server();
 
+let users = [];
+
+const addMessage = message => users.forEach(a => {
+  a.write(message);
+});
+
 server.addService(ChatService.service, {
   send: (request, callback) => {
+    addMessage(request.request);
     callback(null, request.request);
   },
+  join: (call) => {
+    const user = call.request.user;
+    call.on('cancelled', () => {
+      users = users.filter(a => a !== call);
+      addMessage({
+        user: 'System',
+        text: `${user} has left`
+      })
+    });
+    users.push(call);
+    addMessage({
+      user: 'System',
+      text: `${user} has joined!\n\n`
+    })
+  }
 });
 
 server.bindAsync(
